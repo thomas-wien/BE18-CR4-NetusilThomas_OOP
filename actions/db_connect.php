d1e428537e2a8418296fd35a5b1594f43b4e046f
thomas-wien <120489287+thomas-wien@users.noreply.github.com>
2023-03-29 14:07:33 +0200

    Update db_connect.php

--- actions/db_connect.php
+++ actions/db_connect.php
@@ -1,20 +1,12 @@
 <?php
 
 // Defining Constants to use a function also
-if (str_contains($_SERVER['HTTP_HOST'], 'localhost')) {
-    // Local
+ // Local
     DEFINE("HOST", "localhost");
     DEFINE("USER", "root");
     DEFINE("PASS", "");
     DEFINE("DB", "netusilcodefacto_be18_cr4_netusilthomas_biglibrary");
-} else {
-    // Codefactory
-    echo $_SERVER['HTTP_HOST'];
-    DEFINE("HOST", "173.212.235.205");
-    DEFINE("USER", "netusilcodefacto_thomas");
-    DEFINE("PASS", "U9BcyKixxr44fSL");
-    DEFINE("DB", "netusilcodefacto_be18_cr4_netusilthomas_biglibrary");
-}
+
 // create connection
 // $connect = mysqli_connect(HOST, USER, PASS, DB);
 // Object Orientated connection
