e5368e60daaa5dc0f4c8e02605f97e8a1eb78100
Thomas Netusil <thomas.netusil@gmail.com>
2023-03-29 14:45:11 +0200

    modified:   actions/db_connect.php

--- actions/db_connect.php
+++ actions/db_connect.php
@@ -1,19 +1,7 @@
 <?php
 
 // Defining Constants to use a function also
-if (str_contains($_SERVER['HTTP_HOST'], 'localhost')) {
-    // Local
-    DEFINE("HOST", "localhost");
-    DEFINE("USER", "root");
-    DEFINE("PASS", "");
-    DEFINE("DB", "netusilcodefacto_be18_cr4_netusilthomas_biglibrary");
-} else {
-    // Codefactory  
-    DEFINE("HOST", "173.212.235.205");
-    DEFINE("USER", "netusilcodefacto_thomas");
-    DEFINE("PASS", "U9BcyKixxr44fSL");
-    DEFINE("DB", "netusilcodefacto_be18_cr4_netusilthomas_biglibrary");
-}
+require_once 'inc/config.inc.php';
 
 // create connection
 $mysqli = new mysqli(HOST, USER, PASS, DB);
